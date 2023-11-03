ff5a97d69424578bb44095f8c40baf4a95f198bf
jonasnow <125695440+jonasnow@users.noreply.github.com>
2023-11-03 14:30:46 +0100

    nullable

--- database/migrations/2023_11_02_144418_create_companies_table.php
+++ database/migrations/2023_11_02_144418_create_companies_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('companies', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 128)->unique();
+            $table->string('name', 128)->nullable(false)->unique();
             $table->timestamps();
         });
     }
