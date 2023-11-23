d2b4b8d85d4fa7518c726243fc41fbdbf392dbcb
jonasnow <125695440+jonasnow@users.noreply.github.com>
2023-11-23 17:05:41 +0100

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,8 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
-class UserController extends Controller
-{
+class UserController extends Controller {
     public function logout() {
         Auth::logout();
         return redirect('/');
